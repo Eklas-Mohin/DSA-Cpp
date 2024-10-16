15d9685ff2d5b3f73d596d1d1453a6420a69be0a
Eklas-Mohin <mohin030299@gmail.com>
2024-10-16 15:35:06 +0600

    Basic Sorting Algorithm - Bubble Sort

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -13,11 +13,14 @@ int main() {
     cout << endl;
 
     for (int i = 0; i < n - 1; ++i) {
+        int swaps = 0;
         for (int j = 0; j < n - i - 1; ++j) {
             if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);
+                swaps += 1;
             }
         }
+        if (!swaps) break;
     }
     
     cout << "\nSorted Array:" << endl;
