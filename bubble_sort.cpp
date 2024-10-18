0003369466f14a18a548514c8082bf8b6ec9a4bb
Eklas-Mohin <mohin030299@gmail.com>
2024-10-18 17:04:48 +0600

    Basic Sorting Algorithm - Bubble Sort

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,33 +1,42 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int main() {
-    int arr[] = {17, 23, 7, 59, 73, 13, 37, 53, 47};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    cout << "Unsorted Array:" << endl;
-    for (int i = 0; i < n; ++i) {
-        cout << arr[i] << ' ';
-    }
-    cout << endl;
-
-    for (int i = 0; i < n - 1; ++i) {
+void bubbleSort(int arr[], int size) {
+    
+    for (int i = 0; i < size - 1; i++) {
         int swaps = 0;
-        for (int j = 0; j < n - i - 1; ++j) {
+
+        for (int j = 0; j < size - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);
-                swaps += 1;
+                swaps++;
             }
         }
-        if (!swaps) break;
+
+        if (swaps == 0) break;
     }
-    
-    cout << "\nSorted Array:" << endl;
-    for (int i = 0; i < n; ++i) {
-        cout << arr[i] << ' ';
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
     }
-    cout << endl;
+    cout << "\n";
+}
+
+int main() {
+    int arr[] = {17, 23, 7, 59, 73, 13, 37, 53, 47};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Unsorted Array:\n";
+    printArray(arr, size);
+
+    bubbleSort(arr, size);
+
+    cout << "\nSorted Array:\n";
+    printArray(arr, size);
 
     return 0;
 }
